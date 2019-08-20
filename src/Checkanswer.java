e64c27052121d73bb62e08a481d5053398ab4047
James-Cheng-bibi <53065781+James-Cheng-bibi@users.noreply.github.com>
2019-08-20 20:33:16 +1200

    check answer
    we have a answer file.There are right answer in this file,and we will make a array for this answer number.We will also
    make inputlist array.Comparing both array to check answer

---
+++ src/Checkanswer.java
@@ -0,0 +1,16 @@
+
+public class Checkanswer {
+       public void Checkanswer(SudukuTable userinput,SudukuTable answerlist)
+       {
+    	   for(int x=0;x<9;++x)
+    	   {
+    		   for(int y=0;y<9;++y)
+    		   {
+    			   if(userinput.getTable()[x][y].getValue()==answerlist.getTable()[x][y].getValue())
+    			   {
+    				   userinput.getTable()[x][y].setIs_true(true);
+    			   }
+    		   }
+    	   }
+       }
+}
