2bb6deed704c1857b28632037534749de33abae3
Altria4933 <junchen4933@gmail.com>
2019-08-21 14:08:17 +1200

    file io contiunie

--- src/MainFunction.java
+++ src/MainFunction.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -10,6 +11,8 @@ public class MainFunction {
 	public Random randnom;
 	Scanner scan = new Scanner (System.in);
 	boolean gameOn = true;
+	File question = new File ("question.txt");
+	File answer = new File ("answer.txt");
 	
 	
 	public MainFunction () {
@@ -18,18 +21,43 @@ public class MainFunction {
 		display = new printtable();
 	}
 	
+	
+	public void Checkanswer(SudukuTable userinput,SudukuTable answerlist)
+    {
+ 	   for(int x=0;x<9;++x)
+ 	   {
+ 		   for(int y=0;y<9;++y)
+ 		   {
+ 			   if(userinput.getTable()[x][y].getValue()==answerlist.getTable()[x][y].getValue())
+ 			   {
+ 				   userinput.getTable()[x][y].setIs_true(true);
+ 			   }
+ 			   else
+ 			   {
+ 				   userinput.getTable()[x][y].setIs_true(false);
+ 			   }
+ 		   }
+ 	   }
+    }
+
+	
 	public void TestFunction () {
 		tableList = new ArrayList<SudukuTable>();
 		singleTable =  new SudukuTable();
 		display = new printtable();
-		FileIO.ReadList();
+	   FileIO questionIO = new FileIO();
+	  questionIO.setFile(answer);
+	questionIO.ReadList();
 		
-		singleTable = FileIO.getList(9);
+		singleTable = FileIO.getList(0);
 		display.updateTable(singleTable);
 		display.display();
 	
 		
 	}
+	
+	
+	
 	
 	public static void main (String args[]) {
 		MainFunction m = new MainFunction () ;
